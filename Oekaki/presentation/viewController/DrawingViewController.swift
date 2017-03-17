77fc04d3b601e43c4ac02b3662fa3bf08378a4e0
tsk takezawa <tsk.take815@gmail.com>
2017-03-18 04:11:36 +0900

    Add save button

--- Oekaki/presentation/viewController/DrawingViewController.swift
+++ Oekaki/presentation/viewController/DrawingViewController.swift
@@ -2,7 +2,13 @@ import UIKit
 
 final class DrawingViewController: UIViewController {
 
+    @IBOutlet private weak var canvasView: CanvasView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func didTouchSaveButton(_ sender: UIBarButtonItem) {
+        canvasView.clear()
     }
 }
